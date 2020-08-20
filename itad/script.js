20bcd1f9f4af0539f764d83ce77ee16176616f29
anahid <anahid@telf.com>
2020-08-20 17:37:53 -0400

    itad update

--- itad/script.js
+++ itad/script.js
@@ -1,18 +1,22 @@
 function toggleAccordion() {
     $("#panelID").toggleClass("showmenu");
+    $("#page-title").toggleClass("showmenu");
 }
 
-function goToPage(page) {
+function goToPage(page,e) {
     if(page<1 || page>5){
         return;
     }
     currentform = page;
     $("#panelID").removeClass("showmenu");
+    $("#page-title").removeClass("showmenu");
     $(".form-panel").addClass("hidden");
     $("#form-" + page).removeClass("hidden");
 
-    $(".form-selector").removeClass("progress-active");
-    $("#form-selector-" + page).addClass("progress-active");
+    $("[class*='form-selector']").removeClass("progress-active");
+    $(".form-selector-" + page).addClass("progress-active");
+    
+    $("#page-title").text($(e).text());
 }
 
 var currentform = 1;
@@ -24,4 +28,10 @@ function lastPage() {
     goToPage(currentform - 1);
 }
 
-
+function toggleVisibility(e, visible) {
+    if(visible) {
+        $("#"+e).removeClass("hidden");
+    }else {
+        $("#"+e).addClass("hidden");
+    }
+}
